8e7cbc66ae6377269b3daab3f4d45fc063c4753b
ScApeGoAT666 <jmajer@edu.cdv.pl>
2024-02-25 22:33:59 +0100

    x
    x

--- Assets/Scripts/UI/MenuHandler.cs
+++ Assets/Scripts/UI/MenuHandler.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject tutorialPanel;
+
     public void LoadLevel() {
         SceneManager.LoadScene("Levels");
+    }
+
+    public void OpenTuto()
+    {
+        tutorialPanel.SetActive(true);
+    }
+
+    public void CloseTuto()
+    {
+        tutorialPanel.SetActive(false);
+
     }
 }
